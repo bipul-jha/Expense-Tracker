9e4c54ef2f77ec1348a3454eb97bc58ab682c85d
bipul-jha <bipuljha143@gmail.com>
2020-05-24 19:06:51 +0530

    Decorate Transaction List & Add TextBox.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -25,11 +26,12 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomPadding: false,
         appBar: AppBar(
           title: Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Card(
@@ -40,21 +42,69 @@ class MyHomePage extends StatelessWidget {
               ),
               elevation: 5,
             ),
-            Column (children: transactions.map((tx) {
-              return Card(
-                child: Row(
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: <Widget>[
-                    Container(
-                      child: Text(tx.amount.toString()),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
                     ),
-                    Column(children: <Widget>[
-                      Text(tx.title),
-                      Text(tx.date.toString()),
-                    ],)
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                    ),
+                    FlatButton(child: Text('Add Transaction'),
+                    textColor: Colors.purple,
+                    onPressed: () {},
+                    ),
                   ],
                 ),
-              );
-            }).toList(),
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        child: Text(
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(DateFormat.yMMMMd().format(tx.date),
+                              style: TextStyle(color: Colors.grey)),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
           ],
         ));
